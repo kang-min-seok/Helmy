ad5e75e322beb6edb5bfd907ee240e47f37dcdab
kangminseok <min1042004444@gmail.com>
2024-04-25 21:31:19 +0900

    Fix: 안드로이드 버그 수정2

---
+++ lib/models/WorkoutRecord.g.dart
@@ -0,0 +1,170 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'WorkoutRecord.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class WorkoutRecordAdapter extends TypeAdapter<WorkoutRecord> {
+  @override
+  final int typeId = 0;
+
+  @override
+  WorkoutRecord read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return WorkoutRecord(
+      id: fields[0] as int,
+      date: fields[1] as String,
+      workoutTypeIds: (fields[2] as List).cast<int>(),
+      isEdit: fields[3] as bool,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, WorkoutRecord obj) {
+    writer
+      ..writeByte(4)
+      ..writeByte(0)
+      ..write(obj.id)
+      ..writeByte(1)
+      ..write(obj.date)
+      ..writeByte(2)
+      ..write(obj.workoutTypeIds)
+      ..writeByte(3)
+      ..write(obj.isEdit);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is WorkoutRecordAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
+
+class WorkoutTypeAdapter extends TypeAdapter<WorkoutType> {
+  @override
+  final int typeId = 1;
+
+  @override
+  WorkoutType read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return WorkoutType(
+      id: fields[0] as int,
+      name: fields[1] as String,
+      exerciseIds: (fields[2] as List).cast<int>(),
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, WorkoutType obj) {
+    writer
+      ..writeByte(3)
+      ..writeByte(0)
+      ..write(obj.id)
+      ..writeByte(1)
+      ..write(obj.name)
+      ..writeByte(2)
+      ..write(obj.exerciseIds);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is WorkoutTypeAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
+
+class ExerciseAdapter extends TypeAdapter<Exercise> {
+  @override
+  final int typeId = 2;
+
+  @override
+  Exercise read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return Exercise(
+      id: fields[0] as int,
+      name: fields[1] as String,
+      setIds: (fields[2] as List).cast<int>(),
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, Exercise obj) {
+    writer
+      ..writeByte(3)
+      ..writeByte(0)
+      ..write(obj.id)
+      ..writeByte(1)
+      ..write(obj.name)
+      ..writeByte(2)
+      ..write(obj.setIds);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is ExerciseAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
+
+class SetAdapter extends TypeAdapter<Set> {
+  @override
+  final int typeId = 3;
+
+  @override
+  Set read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return Set(
+      id: fields[0] as int,
+      weight: fields[1] as String,
+      reps: (fields[2] as List).cast<String>(),
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, Set obj) {
+    writer
+      ..writeByte(3)
+      ..writeByte(0)
+      ..write(obj.id)
+      ..writeByte(1)
+      ..write(obj.weight)
+      ..writeByte(2)
+      ..write(obj.reps);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is SetAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
